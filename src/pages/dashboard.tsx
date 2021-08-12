74908dc3a8ea8a9e3f8e63226c775b327a44d611
Pedro Kretzschmar <pedro.kretz@gmail.com>
2021-08-11 23:17:08 -0300

    style: added purple ellipse to dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Card from "../components/Card";
 import Input from "../components/Input";
 import Navbar from "../components/Navbar";
@@ -17,11 +18,23 @@ function Dashboard() {
       <Navbar />
       <div className="lg:flex lg:my-8">
         <Card title="token freeze & token thaw" nextPanel="The Economic Engine">
-          <Input name="Opening Price" placeholder="wxDAI" />
-          <Input name="Token Freeze" placeholder="weeks" />
-          <Input name="Token Thaw" placeholder="weeks" />
+          <Input
+            name="Opening Price"
+            placeholder="wxDAI"
+            tooltipText="The Opening Price is the price we sell TEC tokens after the Commons Upgrade is complete."
+          />
+          <Input
+            name="Token Freeze"
+            placeholder="weeks"
+            tooltipText="Token Freeze is the duration from the initialization of the Commons which tokens remain fully locked."
+          />
+          <Input
+            name="Token Thaw"
+            placeholder="weeks"
+            tooltipText="Token Thaw is designed to guarantee, for a certain period, the minimum possible price of the token, or price floor."
+          />
         </Card>
-        <div className="flex flex-col bg-black w-10/12 mx-auto my-4 shadow-2xl lg:w-7/12">
+        <div className="flex flex-col bg-black w-10/12 mx-auto mt-4 shadow-2xl lg:w-7/12">
           <h1 className="font-bj font-bold text-gray-100 text-2xl text-center px-9 pt-6 pb-3 lg:text-left">
             What price should we set the TEC token at launch?
           </h1>
@@ -29,8 +42,8 @@ function Dashboard() {
             The Opening Price is the price we sell TEC tokens after Commons
             Upgrade is complete.
           </h3>
-          <div style={{ height: "350px" }}>chart</div>
-          <div className="min-w-full px-9 py-6 mt-auto">
+          <div className="max-h-80">chart</div>
+          <div className="min-w-full px-9 pt-6 pb-12 mt-auto">
             <div className="flex justify-between font-bj font-bold text-gray-100 text-xs uppercase border-b border-gray-100 pb-2 mb-2">
               <div className="w-1/3 max-w-144"># of tokens</div>
               <div className="w-1/3 max-w-144">% tokens released</div>
@@ -48,6 +61,9 @@ function Dashboard() {
             ))}
           </div>
         </div>
+      </div>
+      <div className="hidden lg:inline fixed left-64 -bottom-16">
+        <Image src="/purple_ellipse.png" width="108" height="108" />
       </div>
     </div>
   );
