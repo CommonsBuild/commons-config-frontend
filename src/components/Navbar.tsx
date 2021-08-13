0f53ab084ccdb675151f200f0c5afc5abccfc401
Pedro Kretzschmar <pedro.kretz@gmail.com>
2021-08-13 19:21:33 -0300

    feat: add advanced settings button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,29 @@
 const modules = [
   {
     id: 1,
-    slug: '',
-    title: 'TOKEN FREEZE & TOKEN THAW',
+    slug: "",
+    title: "TOKEN FREEZE & TOKEN THAW",
   },
   {
     id: 2,
-    slug: '',
-    title: 'AUGMENTED BONDING CURVE',
+    slug: "",
+    title: "AUGMENTED BONDING CURVE",
   },
   {
     id: 3,
-    slug: '',
-    title: 'MODIFYING THE COMMONS',
+    slug: "",
+    title: "MODIFYING THE COMMONS",
   },
   {
     id: 4,
-    slug: '',
-    title: 'REQUESTING FUNDS',
+    slug: "",
+    title: "REQUESTING FUNDS",
   },
 ];
 
 function Navbar() {
   return (
-    <div className="flex bg-black items-center">
+    <div className="flex bg-transparent items-center">
       <div className="bg-neon px-5 py-10">
         <img src="/assets/tec-logo-light.svg" alt="TEC Logomark" />
       </div>
@@ -31,13 +31,16 @@ function Navbar() {
         <img src="/assets/tec-text-light.svg" alt="Token Engineering Commons" />
       </div>
       <div className="hidden lg:flex">
-        {modules.map(({id, title}) => (
+        {modules.map(({ id, title }) => (
           <div className="flex items-center mx-4 text-white" key={id}>
             <span className="text-5xl mr-4">{id}</span>
             <h3 className="text-xs w-28">{title}</h3>
           </div>
         ))}
       </div>
+      <button className="font-bj font-bold text-xs text-neon uppercase ml-auto pr-16">
+        advanced settings
+      </button>
     </div>
   );
 }
