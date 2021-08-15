a921439779f0d924bb5525c2d2a0d7d5fab893a6
Pedro Kretzschmar <pedro.kretz@gmail.com>
2021-08-15 11:47:41 -0300

    feat: integrate chart with backend data

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,23 +1,45 @@
-import Tooltip from './Tooltip';
-import useHover from '../utils/useHover';
+import Tooltip from "./Tooltip";
+import useHover from "../utils/useHover";
 
 interface InputProps {
   name: string;
+  param: string;
   placeholder: string;
   tooltipText: string;
+  value: number;
   changeParam(): void;
+  onChange(): (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-function Input({ name, placeholder, tooltipText, changeParam }: InputProps) {
-  const [hoverRef, isHovered] = useHover<HTMLDivElement>()
+function Input({
+  name,
+  param,
+  placeholder,
+  tooltipText,
+  value,
+  changeParam,
+  onChange,
+}: InputProps) {
+  const [hoverRef, isHovered] = useHover<HTMLDivElement>();
 
   return (
     <div className="px-9 pb-2 text-gray-100 lg:grid lg:grid-cols-2 justify-between">
       <Tooltip text={tooltipText} isHovered={isHovered}>
-        <div ref={hoverRef} className="font-bj self-center py-1">{name}</div>
+        <div ref={hoverRef} className="font-bj self-center py-1">
+          {param}
+        </div>
       </Tooltip>
       <div className="relative h-11">
-        <input onClick={changeParam} maxLength={5} className="transition font-bold text-neon-light text-xl w-full h-full pl-3 border-2 border-gray-500 focus:border-neon hover:border-gray-400 bg-transparent outline-none placeholder-right" />
+        <input
+          name={name}
+          value={value}
+          type="number"
+          min="1"
+          onClick={changeParam}
+          onChange={onChange}
+          maxLength={5}
+          className="transition font-bold text-neon-light text-xl w-full h-full pl-3 border-2 border-gray-500 focus:border-neon hover:border-gray-400 bg-transparent outline-none placeholder-right"
+        />
         <span className="absolute right-3 top-2/4 transform -translate-y-2/4 font-inter text-xs text-gray-300">
           {placeholder}
         </span>
--- src/pages/dashboard/1.tsx
+++ src/pages/dashboard/1.tsx
@@ -1,11 +1,23 @@
-import { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Card from "@/components/Card";
 import Input from "@/components/Input";
 import Navbar from "@/components/Navbar";
 import LineChart from "@/components/LineChart";
+import NeonButton from "@/components/NeonButton";
 
 type ParamsOptionsType = "OPENING_PRICE" | "TOKEN_FREEZE" | "TOKEN_THAW";
+
+interface ParamsValues {
+  "opening-price": number;
+  "token-freeze": number;
+  "token-thaw": number;
+}
+
+interface ChartData {
+  price: number[];
+  week: number[];
+}
 
 const paramsContent = {
   OPENING_PRICE: {
@@ -17,7 +29,7 @@ const paramsContent = {
     question:
       "How long should the Hatcher's TEC tokens be frozen to maintain the price floor?",
     description:
-      "Token Freeze is the duration from the initiliazation of the Commons which tokens remain fully locked.",
+      "Token Freeze is the duration from the initialization of the Commons which tokens remain fully locked.",
   },
   TOKEN_THAW: {
     question:
@@ -28,8 +40,34 @@ const paramsContent = {
 };
 
 function Dashboard() {
+  const [paramsValue, setParamsValue] = useState<ParamsValues>({
+    "opening-price": 2,
+    "token-freeze": 10,
+    "token-thaw": 50,
+  });
+  const [chartData, setChartData] = useState<ChartData>();
+
   const [paramSelected, setParamSelected] =
     useState<ParamsOptionsType>("OPENING_PRICE");
+
+  useEffect(() => {
+    axios
+      .post(
+        "https://commons-config-backend.herokuapp.com/token-lockup/",
+        paramsValue
+      )
+      .then((response) => setChartData(response.data.output));
+  }, [paramsValue]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value ? event.target.value : 1;
+    console.log(value);
+    setParamsValue({
+      ...paramsValue,
+      [name]: value,
+    });
+  };
 
   const tableRows = [
     { time: "5", token: "0", price: "2" },
@@ -39,28 +77,39 @@ function Dashboard() {
     { time: "40", token: "80", price: "0.45" },
     { time: "52", token: "100", price: "0,00" },
   ];
-
   return (
     <div className="lg:min-h-screen bg-dash bg-cover">
       <Navbar />
+      <button className="bg-neon" onClick={() => console.log(paramsValue)}>
+        TESTE
+      </button>
       <div className="lg:flex">
         <Card title="token freeze & token thaw" nextPanel="The Economic Engine">
           <Input
-            name="Opening Price"
+            name="opening-price"
+            value={paramsValue["opening-price"]}
+            param="Opening Price"
             placeholder="wxDAI"
             changeParam={() => setParamSelected("OPENING_PRICE")}
+            onChange={(event) => handleChange(event)}
             tooltipText="The Opening Price is the price we sell TEC tokens after the Commons Upgrade is complete."
           />
           <Input
-            name="Token Freeze"
+            name="token-freeze"
+            value={paramsValue["token-freeze"]}
+            param="Token Freeze"
             placeholder="weeks"
             changeParam={() => setParamSelected("TOKEN_FREEZE")}
+            onChange={(event) => handleChange(event)}
             tooltipText="Token Freeze is the duration from the initialization of the Commons which tokens remain fully locked."
           />
           <Input
-            name="Token Thaw"
+            name="token-thaw"
+            value={paramsValue["token-thaw"]}
+            param="Token Thaw"
             placeholder="weeks"
             changeParam={() => setParamSelected("TOKEN_THAW")}
+            onChange={(event) => handleChange(event)}
             tooltipText="Token Thaw is designed to guarantee, for a certain period, the minimum possible price of the token, or price floor."
           />
         </Card>
@@ -71,8 +120,8 @@ function Dashboard() {
           <h3 className="font-inter text-gray-300 text-center text-xs px-9 pb-6 lg:text-left">
             {paramsContent[paramSelected].description}
           </h3>
-          <LineChart />
-          <div className="min-w-full px-9 pt-4 pb-2 mt-auto">
+          <LineChart price={chartData.price} week={chartData.week} />
+          <div className="min-w-full px-9 pt-4 pb-2">
             <div className="flex justify-between font-bj font-bold text-gray-100 text-xs uppercase border-b border-gray-100 pb-2 mb-2">
               <div className="w-1/3 max-w-144"># of weeks</div>
               <div className="w-1/3 max-w-144">% tokens released</div>
