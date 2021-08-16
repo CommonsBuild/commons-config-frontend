8d9d5dc2b019a6ca5c09c97df75e5ee28fd37e10
Pedro Kretzschmar <pedro.kretz@gmail.com>
2021-08-16 19:55:45 -0300

    style: moved svg icon to public folder

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,8 +6,7 @@ interface InputProps {
   param: string;
   placeholder: string;
   tooltipText: string;
-  value: number;
-  min: number;
+  value: string;
   changeParam(): void;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
@@ -18,33 +17,32 @@ function Input({
   placeholder,
   tooltipText,
   value,
-  min,
   changeParam,
   onChange,
 }: InputProps) {
   const [hoverRef, isHovered] = useHover<HTMLDivElement>();
 
   return (
-    <div className="px-9 pb-2 text-gray-100 lg:grid lg:grid-cols-2 justify-between">
+    <div className="py-2 lg:grid lg:grid-cols-2 justify-between">
       <Tooltip text={tooltipText} isHovered={isHovered}>
-        <div ref={hoverRef} className="font-bj self-center py-1">
+        <span ref={hoverRef} className="font-bj text-gray-100 self-center py-1">
           {param}
-        </div>
+        </span>
       </Tooltip>
       <div className="relative h-11">
         <input
           name={name}
           value={value}
-          type="number"
-          min={min}
           onClick={changeParam}
           onChange={onChange}
           maxLength={5}
-          className="transition font-bold text-neon-light text-xl w-full h-full pl-3 border-2 border-gray-500 focus:border-neon hover:border-gray-400 bg-transparent outline-none placeholder-right"
+          className="font-bold text-neon-light text-xl w-full h-full pl-3 border-2 border-gray-500 focus:border-neon hover:border-gray-400 bg-transparent outline-none placeholder-right"
         />
-        <span className="absolute right-3 top-2/4 transform -translate-y-2/4 font-inter text-xs text-gray-300">
-          {placeholder}
-        </span>
+        <div className="absolute right-3 top-2/4 transform -translate-y-2/4">
+          <span className="font-inter text-xs text-gray-300">
+            {placeholder}
+          </span>
+        </div>
       </div>
     </div>
   );
