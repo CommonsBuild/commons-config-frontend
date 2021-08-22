c5b844f395b08961bbfcbd87d680cc71e54c7ee4
Pedro Kretzschmar <pedro.kretz@gmail.com>
2021-08-22 14:52:07 -0300

    fix: added bg color as props

--- src/components/ChartLegend.tsx
+++ src/components/ChartLegend.tsx
@@ -1,12 +1,13 @@
 interface ChartLegendProps {
   name: string;
-  // bgColor?: string;
+  bgColor?: string;
 }
 
-function ChartLegend({ name }: ChartLegendProps) {
+function ChartLegend({ name, bgColor }: ChartLegendProps) {
   return (
     <div className="flex p-2 my-2 last:col-start-3">
-      <div className="h-4 w-4 bg-neon mx-4" />
+      <div className={`h-4 w-4 mx-4 bg-chart-${bgColor}`} />
+      {/* <div className="h-4 w-4 mx-4 bg-chart-orange" /> */}
       <span className="font-bj font-bold text-neon-light text-xs uppercase">
         {name}
       </span>
