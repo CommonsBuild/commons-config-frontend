421235f7be18cf30c0a7a6233560b1a89199814c
Pedro Kretzschmar <pedro.kretz@gmail.com>
2021-08-25 15:43:28 -0300

    feat: dialog component

---
+++ src/components/Dialog.tsx
@@ -0,0 +1,29 @@
+import classnames from 'classnames';
+
+interface DialogProps {
+  children: React.ReactNode;
+  isOpen: boolean;
+  title: string;
+}
+
+function Dialog({ children, isOpen, title }: DialogProps) {
+  return (
+    <div
+      className={classnames(
+        'absolute h-screen w-screen z-10 bg-black bg-opacity-75 flex justify-center',
+        {
+          hidden: !isOpen,
+        }
+      )}
+    >
+      <div className="bg-black m-auto max-w-lg p-8">
+        <h1 className="font-bj font-bold text-neon-light text-4xl text-center">
+          {title}
+        </h1>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Dialog;
