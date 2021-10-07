c4f9f4af91fbceb0c202a8d99a6ed6ebd3f71c15
Fábio Mendes <ms.fabiomendes@gmail.com>
2021-10-07 13:49:47 -0300

    fix: unused import

--- src/components/charts/TaoVotingBar.tsx
+++ src/components/charts/TaoVotingBar.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
-import ChartLegend from '@/components/ChartLegend';
 import { Tooltip } from '@/components/_global';
 import { useHover } from '@/hooks';
 
@@ -64,7 +63,6 @@ function TaoVotingBar({
   executionDelay,
 }: TaoVotingBarProps) {
   const [hoverRef, isHovered] = useHover<HTMLDivElement>();
-
   const data = {
     labels: [[], [], []],
     datasets: [
