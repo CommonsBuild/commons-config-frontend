6572f14f0cf03cb165035e5ac05eac1de3e986b3
Pedro Kretzschmar <pedro.kretz@gmail.com>
2021-10-12 12:35:39 -0300

    fix: inverted buttons place

--- src/pages/config/2.tsx
+++ src/pages/config/2.tsx
@@ -177,9 +177,13 @@ function ABC() {
                 }
               />
             ))}
-            <RedirectButton href="/learn/2" />
-
-            <div className="py-2">
+            <span
+              className="font-bj font-medium text-neon text-sm py-4 uppercase cursor-pointer"
+              onClick={() => setMarketDialog(true)}
+            >
+              <b>how to use the simulator</b>
+            </span>
+            <div className="pb-2">
               <span className="font-bj font-bold text-neon-light text-xs uppercase">
                 choose your market scenario
               </span>
@@ -263,7 +267,6 @@ function ABC() {
                     />
                   ))}
                 </div>
-
                 <a
                   className="flex justify-center border border-neon-light mt-1"
                   onClick={() => setStepDialog(true)}
@@ -276,13 +279,8 @@ function ABC() {
                   </button>
                 </a>
               </div>
-              <span
-                className="font-bj font-medium text-neon text-sm py-8 uppercase cursor-pointer"
-                onClick={() => setMarketDialog(true)}
-              >
-                <b>how to use the simulator</b>
-              </span>
             </div>
+            <RedirectButton href="/learn/2" />
           </Card>
           <ChartContainer title="Experience your Augmented Bonding Curve by adjusting your parameters and experimenting with the Step Simulator.">
             <ABCChart
