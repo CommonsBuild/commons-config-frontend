89d760545e749b769099cf0f934924783173d1c1
Pedro Kretzschmar <pedro.kretz@gmail.com>
2021-10-15 11:25:02 -0300

    hotfix: zIndex of backdrop component

--- src/components/modals/Backdrop.tsx
+++ src/components/modals/Backdrop.tsx
@@ -9,7 +9,7 @@ function Backdrop({ children, isOpen }: BackdropProps) {
   return (
     <div
       className={classnames(
-        'fixed pin z-50 bg-background h-full w-full flex justify-center items-center',
+        'fixed pin z-100 bg-background h-full w-full flex justify-center items-center',
         {
           hidden: !isOpen,
         }
