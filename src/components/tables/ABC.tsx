b6ff39537a9df3e684545928dc6ab2e8cca1ab77
mitch <divine_comedian@protonmail.com>
2021-10-15 07:07:39 -0600

    change tribute collected header

--- src/components/tables/ABC.tsx
+++ src/components/tables/ABC.tsx
@@ -106,7 +106,7 @@ function ABCTable({ stepList, table }: ABCProps) {
             text="The amount of funds taken from the order and sent to the Common Pool. This is a determined using the parameters entry & exit tribute."
           >
             <span ref={tributeHeader}>
-              tribute collected{' '}
+              tribute{' '}
               <div className="inline-block mt-1">
                 <Image src="/questionMark.svg" height="12px" width="12px" />
               </div>
