6d3873ef14674b8a4bbcbdb377778ab7ca76197b
Pedro Kretzschmar <pedro.kretz@gmail.com>
2021-11-12 18:45:33 -0300

    feat: footer with join param party link

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -35,6 +35,24 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
               <ConvictionVotingProvider>
                 {router.pathname.match('/config/[0-9]') && <Navbar />}
                 <Component {...pageProps} />
+                {router.pathname.match('/config/[0-9]') && (
+                  <footer className="fixed bottom-0 flex justify-center items-center bg-neon w-full h-8">
+                    <div className="marquee">
+                      <div>
+                        <span className="font-bj font-bold text-sm px-16">
+                          Please, keep informed and join our params parties to
+                          develop some nice ECONOMIES with us! Click here to
+                          check our calendar with the time of the events!
+                        </span>
+                        <span className="font-bj font-bold text-sm px-16">
+                          Please, keep informed and join our params parties to
+                          develop some nice ECONOMIES with us! Click here to
+                          check our calendar with the time of the events!
+                        </span>
+                      </div>
+                    </div>
+                  </footer>
+                )}
               </ConvictionVotingProvider>
             </TaoVotingProvider>
           </ABCProvider>
