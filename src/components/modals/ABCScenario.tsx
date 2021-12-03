507148e3058a54e43ed4c612dfbd3c53571669d3
Pedro Kretzschmar <pedro.kretz@gmail.com>
2021-12-03 16:45:02 -0300

    hotfix: how to use simulator text

--- src/components/modals/ABCScenario.tsx
+++ src/components/modals/ABCScenario.tsx
@@ -13,16 +13,16 @@ function ABCScenarioDialog({ handleClose, isOpen }: ABCScenarioDialogProps) {
       title="How to use the ABC Simulator"
     >
       <p className="font-inter text-lg text-neon-light text-center p-4">
-        Use the ABC simulator to see how your configurations react to
-        transactions. Note that Transaction 0 is the initial buy that mints
-        tokens for the Commons itself, and the next 3 transactions are generated
-        automatically.
+        Watch the graph and the table below the graph to see how your
+        configuration reacts to transactions. Note that Transaction 0 is the
+        initial buy that mints tokens for the Commons itself, and the following
+        3 transactions are generated automatically.
       </p>
       <p className="font-inter text-lg text-neon-light text-center p-4">
-        Click the &quot;Add a Transaction&quot; button, to place a buy or sell
-        transaction on your ABC configuration. This will simulate a new
-        transaction on your ABC. Take note that you can use large transactions
-        to explore the full range of the ABC
+        Click the &quot;Simulate a Transaction&quot; button, to place a buy or
+        sell transaction on your ABC configuration. This will add a new
+        transaction on the table and the graph. Take note that you can use large
+        transactions to explore the full range of the ABC.
       </p>
     </Modal>
   );
