dd2c25683d2925fced5a3e463ea7777f45f9fa81
Pedro Kretzschmar <pedro.kretz@gmail.com>
2021-12-06 09:20:36 -0300

    hotfix: change api url

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://dev-commons-config-backend.herokuapp.com/',
+  baseURL: 'https://test-commons-dashboard.herokuapp.com/',
 });
 
 export default api;
